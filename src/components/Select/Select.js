5a989d46efd592b2027dcf98548c90ce5d1c9a3a
Kaz de Groot <kaz@kdds.nl>
2021-09-30 22:40:53 +0200

    Make select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,71 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const SelectContainer = styled.div`
+  position: relative;
+  width: fit-content;
+  border-radius: 8px;
+  z-index: 0;
+  color: ${COLORS.gray700};
+  line-height: 1.2;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectCurrVal = styled.div`
+  border-radius: inherit;
+  padding: 12px 16px;
+  pointer-events: none;
+  position: relative;
+  z-index: 1;
+  background-color: ${COLORS.transparentGray15};
+  padding-right: 60px;
+
+
+`;
+
+const SelectChevronDown = styled(Icon).attrs({size: 36, id: 'chevron-down'})`
+  position: absolute;
+  right: 10px;
+  top: 0;
+  bottom: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+
+const SelectEl = styled.select`
+  border: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  left: 0;
+  border-radius: inherit;
+  margin: 0;
+  z-index: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 100%;
+  color: rgba(0, 0, 0, 0);
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectContainer>
+      <SelectEl value={value} onChange={onChange}>
+        {children}
+      </SelectEl>
+      <SelectCurrVal>
+        {displayedValue}  
+        <SelectChevronDown />
+      </SelectCurrVal>
+    </SelectContainer>
   );
 };
 
