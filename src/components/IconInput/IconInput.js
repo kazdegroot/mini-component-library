cee2504bddfd0375e0bfcddd4cdb374d2a162a3d
Kaz de Groot <kaz@kdds.nl>
2021-09-30 23:06:00 +0200

    Make iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,63 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--underline-size': '1px',
+    '--font-size': '14px',
+    '--padding': '2px 2px 2px 20px',
+    iconSize: 14,
+  },
+}
+
+const Container = styled.label`
+  --underline-size: 2px;
+  --font-size: 18px;
+  --padding: 5px 5px 5px 30px;
+
+  line-height: 1.17;
+  position: relative;
+  display: block;
+  font-size: var(--font-size);
+  font-family: 'Roboto', sans-serif;
+  border-bottom: var(--underline-size) solid;
+  width: ${p => p.$width}px;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Input = styled.input`
+  border: 0;
+  margin: 0;
+  padding: var(--padding);
+  width: 100%;
+  font-weight: 700;
+  font-family: inherit;
+  line-height: inherit;
+  font-size: inherit;
+  color: inherit;
+
+  &:focus {
+    outline-offset: 1px;
+  }
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500}
+  }
+`;
+
+const FieldIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto 0;
+`;
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +70,12 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const { iconSize = 22, ...style } = SIZES[size] || {};
+  return <Container $width={width} style={style}>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    {icon ? <FieldIcon id={icon} size={iconSize} /> : null }
+    <Input placeholder={placeholder}/>
+  </Container>;
 };
 
 export default IconInput;
